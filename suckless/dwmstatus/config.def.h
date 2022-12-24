81f63e431dd3a63d84e4557f9ef24033c7dd3e36
r3duc3 <itsreduce@gmail.com>
2022-12-24 14:40:52 +0700

    dwmstatus

---
+++ suckless/dwmstatus/config.def.h
@@ -0,0 +1,4 @@
+#define TZ_NAME "Asia/Jakarta"
+#define TZ_FORMAT "%a, %d/%m %H:%M:%S"
+
+#define BAT "/sys/class/power_supply/BAT1"
---
+++ suckless/dwmstatus/dwmstatus.c
@@ -0,0 +1,196 @@
+/*
+ * Copy me if you can.
+ * by 20h
+ */
+
+#define _BSD_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <X11/Xlib.h>
+#include "config.h"
+
+static Display *dpy;
+
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
+
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
+
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
+
+	return ret;
+}
+
+char *
+mktimes()
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	settz(TZ_NAME);
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (timtm == NULL)
+		return smprintf("");
+
+	if (!strftime(buf, sizeof(buf)-1, TZ_FORMAT, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		return smprintf("");
+	}
+
+	return smprintf("%s", buf);
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+char *
+readfile(char *base, char *file)
+{
+	char *path, line[513];
+	FILE *fd;
+
+	memset(line, 0, sizeof(line));
+
+	path = smprintf("%s/%s", base, file);
+	fd = fopen(path, "r");
+	free(path);
+	if (fd == NULL)
+		return NULL;
+
+	if (fgets(line, sizeof(line)-1, fd) == NULL)
+		return NULL;
+	fclose(fd);
+
+	return smprintf("%s", line);
+}
+
+char *
+getbattery()
+{
+	char *co, status;
+	int descap, remcap;
+
+	descap = -1;
+	remcap = -1;
+
+	co = readfile(BAT, "present");
+	if (co == NULL)
+		return smprintf("");
+	if (co[0] != '1') {
+		free(co);
+		return smprintf("not present");
+	}
+	free(co);
+
+	co = readfile(base, "charge_full");
+	if (co == NULL) {
+		co = readfile(base, "energy_full");
+		if (co == NULL)
+			return smprintf("");
+	}
+	sscanf(co, "%d", &descap);
+	free(co);
+
+	co = readfile(base, "charge_now");
+	if (co == NULL) {
+		co = readfile(base, "energy_now");
+		if (co == NULL)
+			return smprintf("");
+	}
+	sscanf(co, "%d", &remcap);
+	free(co);
+
+	co = readfile(base, "status");
+	if (!strncmp(co, "Discharging", 11)) {
+		status = '-';
+	} else if(!strncmp(co, "Charging", 8)) {
+		status = '+';
+	} else {
+		status = '?';
+	}
+
+	if (remcap < 0 || descap < 0)
+		return smprintf("invalid");
+
+	return smprintf("%.0f%%%c", ((float)remcap / (float)descap) * 100, status);
+}
+
+char *
+execscript(char *cmd)
+{
+	FILE *fp;
+	char retval[1025], *rv;
+
+	memset(retval, 0, sizeof(retval));
+
+	fp = popen(cmd, "r");
+	if (fp == NULL)
+		return smprintf("");
+
+	rv = fgets(retval, sizeof(retval), fp);
+	pclose(fp);
+	if (rv == NULL)
+		return smprintf("");
+	retval[strlen(retval)-1] = '\0';
+
+	return smprintf("%s", retval);
+}
+
+int
+main(void)
+{
+	char *status;
+	char *baty;
+	char *ltime;
+
+	if (!(dpy = XOpenDisplay(NULL))) {
+		fprintf(stderr, "dwmstatus: cannot open display.\n");
+		return 1;
+	}
+
+	for (;;sleep(1)) {
+		baty = getbattery();
+		ltime = mktimes();
+
+		status = smprintf("B:%s %s", baty, ltime);
+		setstatus(status);
+
+		free(status);
+		free(baty);
+		free(ltime);
+	}
+
+	XCloseDisplay(dpy);
+
+	return 0;
+}
+
