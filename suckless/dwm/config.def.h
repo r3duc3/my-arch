294edce0084adfccaf7bbc97c360e6d0b7f37f84
_Reduce <itsreduce@proton.me>
2023-01-11 18:13:06 +0700

    update

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -49,7 +49,6 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
